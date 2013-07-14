2b503846ea63a7cadc0175c932e34ee334c30b1c
Karl Groves <karl@simplyaccessible.com>
2013-07-14 12:27:33 -0400

    unzip of blankslate theme and placement of it in the themese directory

---
+++ wp-content/themes/blankslate/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title(' | ', true, 'right'); ?></title>
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrapper" class="hfeed">
+<header>
+<div id="branding">
+<div id="site-title"><?php if ( is_singular() ) {} else {echo '<h1>';} ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="home"><?php bloginfo( 'name' ) ?></a><?php if ( is_singular() ) {} else {echo '</h1>';} ?></div>
+<p id="site-description"><?php bloginfo( 'description' ) ?></p>
+</div>
+<nav>
+<div id="search">
+<?php get_search_form(); ?>
+</div>
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+</nav>
+</header>
+<div id="container">
