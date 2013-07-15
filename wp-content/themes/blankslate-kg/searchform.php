c6ed8c282f45fa889bba7ff8c8f7d42a51c54530
Karl Groves <karl@simplyaccessible.com>
2013-07-15 07:28:02 -0400

    Tons of formatting changes for readability. Added review folder with static pages

---
+++ wp-content/themes/blankslate-kg/searchform.php
@@ -0,0 +1,7 @@
+<form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+	<div id="search-inputs">
+	    <!--KLG - changed input type to search-->
+		<input type="search" value="" name="s" id="s" />
+		<input type="submit" id="searchsubmit" value="Search" />
+	</div>
+</form>
